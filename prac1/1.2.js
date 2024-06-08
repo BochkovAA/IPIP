6b26e0bf1e87583caa454fdba9148721a6d20e3a
BochkovAA <135096677+BochkovAA@users.noreply.github.com>
2024-06-08 07:57:09 +0400

    Практическая 1, задание 2

---
+++ prac1/1.2.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 40; i++) {
+    console.log(i);
+}
+
+let j = 1;
+while (j <= 40) {
+    console.log(j);
+    j++;
+}
+
+let k = 1;
+do {
+    console.log(k);
+    k++;
+} while (k <= 40);
+
